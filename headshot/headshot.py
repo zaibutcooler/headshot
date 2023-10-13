db976778f1f0ee26a61ec9739c434d002ced7244
Zai <130903099+zaiYellYintAung@users.noreply.github.com>
2023-10-14 03:17:15 +0630

    project init

---
+++ headshot/headshot.py
@@ -0,0 +1,9 @@
+import torch
+from torch import nn
+
+class Headshot(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self,x):
+        pass
---
+++ interface/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+def main():
+    st.title("Head shot")
+    st.write("Simulate wartime and get the headshot using python")
+    
+    
+if __name__ == '__main__':
+    main()
