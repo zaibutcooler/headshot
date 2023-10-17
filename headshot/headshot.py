18943e53647090efbfe74a8a226f50047791566f
Zai <130903099+zaiYellYintAung@users.noreply.github.com>
2023-10-17 12:03:06 +0630

    robot.py and dataset starting

--- headshot/headshot.py
+++ headshot/headshot.py
@@ -1,9 +1,27 @@
 import torch
 from torch import nn
+from data_prep import FaceDataset
 
 class Headshot(nn.Module):
     def __init__(self):
         super().__init__()
+        self.dataset = FaceDataset()
+        self.num_epoch = 20
 
     def forward(self,x):
         pass
+
+    def train(self):
+        for epoch in range(self.num_epoch):
+            for i,(image,label) in enumerate(self.dataset):
+                pass
+
+    def predict(self,image):
+        points = self.forward(image)
+        
+
+    def load_pretrain(self,name=""):
+        pretrained = torch.load(pretrained)
+        self.load_state_dict(pretrained)
+
+    
