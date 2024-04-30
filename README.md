dae01361869e5f3886f645f0aebb447ac1c81550
Zai <130903099+zaibutcooler@users.noreply.github.com>
2024-05-01 00:54:16 +0630

    syncing with space

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Headshot Project
+---
 
+title: Headshot
+emoji: ✨
+colorFrom: purple
+colorTo: blue
+sdk: streamlit
+sdk_version: 1.29.0
+app_file: space.py
+pinned: false
+license: openrail
+---
+
+
+# Headshot Project
--- headshot/data_prep.py
+++ headshot/data_prep.py
@@ -3,21 +3,21 @@ from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms, datasets
 from datasets import load_dataset
 
-data_url = ''
-
-datas = None
-labels = None
-
 class FaceDataset(Dataset):
     def __init__(self):
         self.tranforms = transforms.Compose([
 
         ])
-        self.data = datas
-        self.labels = labels
+        
+        self.data = None
+
 
     def __len__(self):
         return len(self.data)
+
+    def download_data(self):
+        loaded_data = load_dataset('zaibutcooler/chimpanzees')
+        self.data = loaded_data
 
     def __getitem__(self, idx):
         # Load and preprocess the image at the given index
